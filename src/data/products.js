e12924935bb18dfc62ac07f0995a26170f80f8f8
John Doe <johndoe@example.com>
2023-09-13 16:17:27 +0300

    fix

---
+++ src/data/products.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: 'sdsjkdsghjdsghjdsgh',
+    prise: 5000,
+    image: 'https://i.pinimg.com/originals/5b/24/1e/5b241ee76bc7e8879f0bc7a1e6d249f7.jpg',
+  },
+  {
+    title: 'sdsjkdsghjdsghjdsgh',
+    prise: 5000,
+    image: 'https://i.pinimg.com/originals/5b/24/1e/5b241ee76bc7e8879f0bc7a1e6d249f7.jpg',
+  },
+  {
+    title: 'sdsjkdsghjdsghjdsgh',
+    prise: 1000,
+    image: 'https://vagabondish.com/wp-content/uploads/anker-astro3-external-battery.jpg',
+  },
+  {
+    title: 'sdsjkdsghjdsghjdsgh',
+    prise: 8888,
+    image: 'http://img1.kakaku.k-img.com/Images/prdnews/2017031/20170313122831_862_.jpg',
+  },
+  {
+    title: 'sdsjkdsghjdsghjdsgh',
+    prise: 999,
+    image: 'https://i.pinimg.com/736x/f0/73/0d/f0730dd5a9204b8fa72f8a39808c2176.jpg',
+  },
+  {
+    title: 'sdsjkdsghjdsghjdsgh',
+    prise: 33333,
+    image: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2018/04/298571-smartwatch-fitbit-versa.jpg?tf=3840x',
+  },
+];
